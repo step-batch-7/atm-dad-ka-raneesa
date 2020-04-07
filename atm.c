ff6dcdf10989f0c19bdc0e1ffc5808620c632bea
Deepika <bhuvanadeepika017@gmail.com>
2020-04-07 20:13:34 +0530

    added code to get_money function to return 2000 denomination notes

--- atm.c
+++ atm.c
@@ -4,6 +4,11 @@
 unsigned int get_money(unsigned short int money)
 {
   unsigned int note_count = 0x00000000;
-  
+  unsigned short int remaining_balance = money;
+  unsigned int no_of_notes = 0;
+
+  no_of_notes = remaining_balance / 2000;
+  remaining_balance = remaining_balance % 2000;
+  note_count = note_count + (0x10000000 * no_of_notes);
   return note_count;
 }
