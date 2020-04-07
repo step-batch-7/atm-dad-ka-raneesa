4ce3b2e0cfb0fe02ea445d9f83b099fe29f94afb
Deepika <bhuvanadeepika017@gmail.com>
2020-04-07 20:18:09 +0530

    modified get_money function

--- atm.c
+++ atm.c
@@ -8,7 +8,7 @@ unsigned int get_money(unsigned short int money)
   unsigned int no_of_notes = 0;
 
   no_of_notes = remaining_balance / 2000;
-  remaining_balance = remaining_balance % 2000;
-  note_count = note_count + (0x10000000 * no_of_notes);
+  note_count = note_count | no_of_notes;
+  note_count = note_count << 28;
   return note_count;
 }
